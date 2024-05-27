c9a537849b69cbbe42fbb7ce2e3471ee4f6f9c0a
FranciscoCHANG <francisco940513@gmail.com>
2024-05-28 00:19:38 +0800

    google 登入所需的一些內容

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,17 @@ import logo from '@/public/img/logo.png'
 import styles from '@/styles/Home.module.scss'
 
 export default function Home() {
+
+  const handleGoogleLogin = () => {
+    // 將這個URL發送給前端，用戶點擊後將重定向到Google登入頁面
+    window.location.href = '/api/login';
+  };
+
   return (
     <Layout>
       <main className={styles.login}>
         <Image src={logo} alt='logo' />
-        <a className={styles.loginButtton}><FaGoogle /></a>
+        <a className={styles.loginButtton} onClick={handleGoogleLogin}><FaGoogle/></a>
       </main>
     </Layout>
   );
