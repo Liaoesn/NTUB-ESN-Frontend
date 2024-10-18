6905378c41d90cfc5aa5d715c85c45838c236f33
Seavetr <aina174174@gmail.com>
2024-10-18 23:18:13 +0800

    改歷年

--- pages/project/[prono].tsx
+++ pages/project/[prono].tsx
@@ -9,6 +9,7 @@ export default function ShowHistory({ user }: { user: userI | undefined }) {
 
   const [studentItems, setItems] = useState<proItemInterface[]>([]);
   const [description, setDescription] = useState<string>("");
+  const [context, setContext] = useState<string>("");
   // api 取得 permissions 的 mapping 清單
   useEffect(() => {
     const fetchData = async () => {
@@ -32,9 +33,38 @@ export default function ShowHistory({ user }: { user: userI | undefined }) {
     setIsVisible(id);
 
     const studentItem = studentItems.find(item => item.final_ranking === id);
-    var description = studentItem?.type + " " + studentItem?.value + " " + studentItem?.description;
 
+    if(studentItem) {
+
+      var context = `
+    讀書計畫：${studentItem.studyplan}
+    機會：${studentItem.opportunity}
+    自我期許：${studentItem.selfexpectation}
+    工作背景：${studentItem.work}
+    多元學習：${studentItem.learning}
+    家庭背景：${studentItem.family}
+    教育背景：${studentItem.education}
+    求學經歷：${studentItem.studyexperience}
+    推薦人：${studentItem.recommender}
+    學歷：${studentItem.edu}
+    證照：${studentItem.licence}
+    工讀：${studentItem.pt}
+    服務及學習：${studentItem.servelearn}
+    其他資訊：${studentItem.other}
+    家庭狀況：${studentItem.home}
+    基本資料：${studentItem.basic}
+    技能：${studentItem.skill}
+    `;
+
+    var description = `
+    內容類型：${studentItem.studyplan}
+    詳細資料：${studentItem.description}
+    實際值：${studentItem.value}
+    `;
+
+    setContext(context);
     setDescription(description);
+    }
   };
   
   return (
@@ -64,8 +94,10 @@ export default function ShowHistory({ user }: { user: userI | undefined }) {
 
           <article className={styles.aboutArea}>
             <p className={styles.aboutContext}>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              {description}
+            {context}
+            </p>
+            <p className={styles.aboutDescription}>
+            {description}
             </p>
             <div className={styles.aboutProject}>
               <div className={styles.title}>
