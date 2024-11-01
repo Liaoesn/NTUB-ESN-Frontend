3171d913303e89b04ec467b366135aedeca6fde0
Esn <10956023@ntub.edu.tw>
2024-11-01 20:49:52 +0800

    新增-修到一半

--- pages/projectManage/add.tsx
+++ pages/projectManage/add.tsx
@@ -3,16 +3,12 @@ import styles from '@/styles/page/project/add.module.scss';
 import React, { useState } from 'react';
 import PrivateRoute from '../privateRoute';
 import userI from '../../type/userI';
-import ProjectName from '@/components/popup/addproject/addName';
-import ProjectPeople from '@/components/popup/addproject/addPeople';
-import ProjectChoose from '@/components/popup/addproject/addChoose';
-import ProjectFile from '@/components/popup/addproject/addFile';
-import ProjectTime from '@/components/popup/addproject/addTime';
-import { FaCheck } from 'react-icons/fa';
+import { FaCheck, FaFolder, FaPen, FaRegCalendarAlt, FaSignOutAlt } from 'react-icons/fa';
 import { useRouter } from 'next/router';
 import FailPopup from '@/components/popup/failPopup';
 import { MdArrowLeft } from 'react-icons/md';
 import axios from 'axios';
+import { Autocomplete, TextField } from '@mui/material';
 function CreateProject({ user }: { user: userI | undefined }) {
   const [showPopup, setShowPopup] = useState(false);
   const [popupTitle, setPopupTitle] = useState('')
@@ -23,33 +19,34 @@ function CreateProject({ user }: { user: userI | undefined }) {
   const [endDate, setEndDate] = useState('');
   const [files, setFiles] = useState<FileList | null>(null);
   const [people, setPeople] = useState<number>(0);
-  const [type, setType] = useState('1');
+  const [type, setType] = useState('');
+
   const handleSubmit = () => {
-    if (college.length < 1 || projectName.length < 1 ){
+    if (college.length < 1 || projectName.length < 1) {
       setPopupTitle('第一步驟有空值')
       setShowPopup(true);
       setTimeout(() => {
         setShowPopup(false);
       }, 2000);
-    } else if (startDate.length < 1 || endDate.length < 1 ){
+    } else if (startDate.length < 1 || endDate.length < 1) {
       setPopupTitle('第二步驟有空值')
       setShowPopup(true);
       setTimeout(() => {
         setShowPopup(false);
       }, 2000);
-    } else if ( files == null ){
+    } else if (files == null) {
       setPopupTitle('第三步驟有空值')
       setShowPopup(true);
       setTimeout(() => {
         setShowPopup(false);
       }, 2000);
-    } else if (people < 1 ){
+    } else if (people < 1) {
       setPopupTitle('第四步驟有空值')
       setShowPopup(true);
       setTimeout(() => {
         setShowPopup(false);
       }, 2000);
-    } else if ( type.length < 1 ){
+    } else if (type.length < 1) {
       setPopupTitle('第五步驟有空值')
       setShowPopup(true);
       setTimeout(() => {
@@ -58,7 +55,7 @@ function CreateProject({ user }: { user: userI | undefined }) {
     } else {
       setShowPopup(true);
     }
-    console.log([college,projectName,startDate,endDate,files,people,type])
+    console.log([college, projectName, startDate, endDate, files, people, type])
     console.log({
       college,
       projectName,
@@ -68,144 +65,74 @@ function CreateProject({ user }: { user: userI | undefined }) {
       endDate,
     });
   };
-  const [showName, setShowName] = React.useState(true);
-  const [showTime, setShowTime] = React.useState(false);
-  const [showFile, setShowFile] = React.useState(false);
-  const [showPeople, setShowPeople] = React.useState(false);
-  const [showChoose, setShowChoose] = React.useState(false);
-  const [showLast, setShowLast] = React.useState(false);
-  const popupNext = (popupName: string) => {
-    switch (popupName) {
-      case 'Name':
-        setShowName(prev => !prev);
-        setShowTime(prev => !prev);
-        break;
-      case 'Time':
-        setShowTime(prev => !prev);
-        setShowFile(prev => !prev);
-        break;
-      case 'File':
-        setShowFile(prev => !prev);
-        setShowPeople(prev => !prev);
-        break;
-      case 'People':
-        setShowPeople(prev => !prev);
-        setShowChoose(prev => !prev);
-        break;
-      case 'Chose':
-        setShowChoose(prev => !prev);
-        setShowLast(prev => !prev);
-        break;
-      default:
-        break;
-    }
-  };
-  const popupBack = (popupName: string) => {
-    switch (popupName) {
-      case 'Time':
-        setShowName(prev => !prev);
-        setShowTime(prev => !prev);
-        break;
-      case 'File':
-        setShowTime(prev => !prev);
-        setShowFile(prev => !prev);
-        break;
-      case 'People':
-        setShowFile(prev => !prev);
-        setShowPeople(prev => !prev);
-        break;
-      case 'Chose':
-        setShowPeople(prev => !prev);
-        setShowChoose(prev => !prev);
-        break;
-      case 'Last':
-        setShowChoose(prev => !prev);
-        setShowLast(prev => !prev);
-        break;
-      case 'ok':
-        setShowChoose(prev => !prev);
-        setShowLast(prev => !prev);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handelWaiting = (popupName: string) => {
-    if (router.isReady) {
-      popupNext(popupName)
-    }
-  }
-
-  const onSubmit = () => {
-    axios.post('/api/project/insert', {
-      'proname': projectName,                  // 專案名稱
-      'prodescription': college,     // 專案描述
-      'startdate': new Date().toISOString().split('T')[0],             // 開始日期，格式 YYYY-MM-DD
-      'phase1': startDate,                // 第一階段日期
-      'enddate': endDate,               // 結束日期，格式 YYYY-MM-DD
-      'userno': user?.userno,                     // 負責人的使用者編號
-      'admissions': 30,                      // 錄取人數
-      'share_type': type
-    })
-    .then(response => {
-      console.log('專案新增成功:', response.data);
-      router.push('/projectManage/list')
-    })
-    .catch(error => {
-      console.error('新增專案時發生錯誤:', error);
-    });
-  }
-
   return (
     <>
       <Layout>
-        <main className={styles.coverBg}>
-          <section className={styles.projectForm}>
-            {showName &&
-              <ProjectName
-                College={setCollege} ProjectName={setProjectName}
-                CollegeValue={college as string} ProjectNameValue={projectName}
-                onNext={() => handelWaiting('Name')} />}
-            {showTime &&
-              <ProjectTime
-                StartTime={setStartDate} EndTime={setEndDate}
-                StartTimeValue={startDate} EndTimeValue={endDate}
-                onNext={() => popupNext('Time')}
-                onBack={() => popupBack('Time')} />}
-            {showFile &&
-              <ProjectFile
-                File={setFiles}
-                onNext={() => popupNext('File')}
-                onBack={() => popupBack('File')} />}
-            {showPeople &&
-              <ProjectPeople
-                People={setPeople}
-                onNext={() => popupNext('People')}
-                onBack={() => popupBack('People')} />}
-            {showChoose &&
-              <ProjectChoose
-                Type={setType}
-                submit={handleSubmit}
-                onNext={() => popupNext('Chose')}
-                onBack={() => popupBack('Chose')}/>}
-            {showLast && <div className={styles.popupBG}>
-              <div className={styles.mainShow}>
-                <section className={styles.title}>
-                  <h4>結束設定</h4>
-                </section>
-                <section className={styles.inputArea}>
-                  請確認都有完成輸入
-                </section>
-                <div className={styles.bottom}>
-                  <a onClick={() => popupBack('ok')} className={styles.left}><MdArrowLeft /></a>
-                  <a onClick={onSubmit} className={`${styles.button} ${styles.check}`} >
-                    <FaCheck />
-                  </a>
-                </div>
+        <main className={styles.addArea}>
+          <h2>新曾專案</h2>
+          <section className={styles.add}>
+            <div className={styles.setingArea}>
+              <Autocomplete
+                sx={{ m: 2, height:'54px', backgroundColor: '#e0f7fa', borderRadius: '25px'}}
+                className={styles.addInput}
+                id="free-solo-demo"
+                freeSolo options={[]}
+                value={projectName}
+                onInputChange={(e) => setProjectName}
+                renderInput={(params) => <TextField className={styles.addInput} {...params} label="輸入專案名稱" />}
+              />
+            </div>
+            <div className={styles.setingList}>
+              <div className={styles.contentShort}>
+                <h3>學制</h3>
+                {/* <p>{projectData ? projectData.prodescription : "Loading..."}</p> */}
               </div>
-            </div>}
-          { showPopup ? <FailPopup title={popupTitle}/> : '' }
+              <a
+              //  onClick={() => handlePopup('Name')}
+              ><FaPen /></a>
+              <div className={styles.contentShort}>
+                <h3>審核方式</h3>
+                {/* <p>{projectData ? projectData.share_type == '1' ? '全部分配' : '平均分配' : "Loading..."}</p> */}
+              </div>
+              <a
+              //  onClick={() => handlePopup('Chose')}
+              ><FaPen /></a>
+            </div>
+            <div className={styles.setingList}>
+              <div className={styles.contentShort}>
+                <h3>資料數量</h3>
+                {/* <p>{fileNumber ? fileNumber.total_students : "Loading..."}</p> */}
+              </div>
+              <a
+              //  onClick={() => handlePopup('File')}
+              ><FaFolder /></a>
+              <div className={styles.contentShort}>
+                <h3>錄取人數</h3>
+                {/* <p>{projectData ? projectData.admissions : "Loading..."}</p> */}
+              </div>
+              <a
+              //  onClick={() => handlePopup('People')}
+              ><FaPen /></a>
+            </div>
+            <div className={styles.setingList}>
+              <div className={styles.contentShort}>
+                <h3>第一階段</h3>
+                <p>2024/09/28</p>
+              </div>
+              <a
+              //  onClick={() => handlePopup('Time')}
+              ><FaRegCalendarAlt /></a>
+              <div className={styles.contentShort}>
+                <h3>結束日期</h3>
+                <p>2024/11/11</p>
+              </div>
+              <a
+              //  onClick={() => handlePopup('Time')}
+              ><FaRegCalendarAlt /></a>
+            </div>
+          </section>
+          <section className={styles.setingButton}>
+            <a className={`${styles.button} ${styles.check}`}><FaSignOutAlt /></a>
           </section>
         </main>
       </Layout>
