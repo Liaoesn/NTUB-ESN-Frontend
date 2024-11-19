80ba540f9b85b9da9e5a124d299f3023c102b31b
Eason <10956023@ntub.edu.tw>
2024-11-20 00:12:09 +0800

    修改-新增

--- pages/projectManage/add.tsx
+++ pages/projectManage/add.tsx
@@ -3,16 +3,14 @@ import styles from '@/styles/page/project/add.module.scss';
 import React, { useState } from 'react';
 import PrivateRoute from '../privateRoute';
 import userI from '../../type/userI';
-import { FaCheck, FaFolder, FaPen, FaRegCalendarAlt, FaSignOutAlt } from 'react-icons/fa';
+import { FaCheck, FaSignOutAlt } from 'react-icons/fa';
+import { MdFileUpload } from 'react-icons/md';
+import { Autocomplete, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import axios from 'axios';
 import { useRouter } from 'next/router';
 import FailPopup from '@/components/popup/failPopup';
-import { MdArrowLeft, MdFileUpload } from 'react-icons/md';
-import axios from 'axios';
-import { Autocomplete, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 
 function CreateProject({ user }: { user: userI | undefined }) {
-  const [showPopup, setShowPopup] = useState(false);
-  const [popupTitle, setPopupTitle] = useState('')
   const router = useRouter();
   const [college, setCollege] = useState('');
   const [people, setPeople] = useState<number>();
@@ -22,17 +20,19 @@ function CreateProject({ user }: { user: userI | undefined }) {
   const today = new Date().toISOString().split('T')[0];
   const [filesName, setFilesName] = useState<string>('點擊或拖動文件到這裡上傳');
   const [files, setFiles] = useState<FileList | null>(null);
-  const [type, setType] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupTitle, setPopupTitle] = useState('')
+
   const handleStartDateChange = (e: any) => {
-      setStartDate(e.target.value);
-      // 清空第二個日期選擇器，防止選擇不符合規則的日期
-      if (endDate && e.target.value > endDate) {
-          setEndDate('');
-      }
+    setStartDate(e.target.value);
+    // 清空第二個日期選擇器，防止選擇不符合規則的日期
+    if (endDate && e.target.value > endDate) {
+      setEndDate('');
+    }
   };
 
   const handleEndDateChange = (e: any) => {
-      setEndDate(e.target.value);
+    setEndDate(e.target.value);
   };
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,55 +40,77 @@ function CreateProject({ user }: { user: userI | undefined }) {
     setFilesName(event.target.files ? event.target.files[0].name : '');
   };
 
-  // const handleSubmit = () => {
-  //   if (college.length < 1 || projectName.length < 1) {
-  //     setPopupTitle('第一步驟有空值')
-  //     setShowPopup(true);
-  //     setTimeout(() => {
-  //       setShowPopup(false);
-  //     }, 2000);
-  //   } else if (startDate.length < 1 || endDate.length < 1) {
-  //     setPopupTitle('第二步驟有空值')
-  //     setShowPopup(true);
-  //     setTimeout(() => {
-  //       setShowPopup(false);
-  //     }, 2000);
-  //   } else if (files == null) {
-  //     setPopupTitle('第三步驟有空值')
-  //     setShowPopup(true);
-  //     setTimeout(() => {
-  //       setShowPopup(false);
-  //     }, 2000);
-  //   } else if (people < 1) {
-  //     setPopupTitle('第四步驟有空值')
-  //     setShowPopup(true);
-  //     setTimeout(() => {
-  //       setShowPopup(false);
-  //     }, 2000);
-  //   } else if (type.length < 1) {
-  //     setPopupTitle('第五步驟有空值')
-  //     setShowPopup(true);
-  //     setTimeout(() => {
-  //       setShowPopup(false);
-  //     }, 2000);
-  //   } else {
-  //     setShowPopup(true);
-  //   }
-  //   console.log([college, projectName, startDate, endDate, files, people, type])
-  //   console.log({
-  //     college,
-  //     projectName,
-  //     type,
-  //     people,
-  //     startDate,
-  //     endDate,
-  //   });
-  // };
-  
+  const handleSubmit = async () => {
+    console.log([college, projectName, startDate, endDate, files, people])
+    if (projectName.length < 1) {
+      setPopupTitle('專案名稱不可為空')
+      setShowPopup(true);
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 3500);
+    } else if (college.length < 1) {
+      setPopupTitle('學制型態不可為空')
+      setShowPopup(true);
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 3500);
+    } else if (!people) {
+      setPopupTitle('錄取人數不可為空')
+      setShowPopup(true);
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 3500);
+    } else if (people < 1) {
+      setPopupTitle('錄取人數不可為0')
+      setShowPopup(true);
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 3500);
+    } else if (startDate.length < 1) {
+      setPopupTitle('評分截止日期不可為空')
+      setShowPopup(true);
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 3500);
+    } else if (endDate.length < 1) {
+      setPopupTitle('專案截止日期不可為空')
+      setShowPopup(true);
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 3500);
+    } else if (filesName == '點擊或拖動文件到這裡上傳') {
+      setPopupTitle('上傳資料不可為空')
+      setShowPopup(true);
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 3500);
+    } else {
+      console.log('123456')
+      try {
+        const response = await axios.post('/api/project/insert', 
+          {
+            'proname': projectName, 
+            'prodescription': college, 
+            'admissions': people, 
+            'phase1': startDate, 
+            'user_no': user?.user_no,
+            'enddate': endDate, 
+        }
+        );
+        console.log('專案新增成功：', response.data);
+        alert('專案新增成功');
+      } catch (error) {
+        // console.error('新增專案失敗：', error?.response?.data || error.message);
+        alert('新增專案失敗，請檢查輸入資料！');
+      }
+    }
+  };
+
   return (
-    
+
     <>
       <Layout>
+        {showPopup ? <FailPopup title={popupTitle} /> : ''}
         <main className={styles.addArea}>
           <h2>新增專案</h2>
           <section className={styles.add}>
@@ -99,7 +121,7 @@ function CreateProject({ user }: { user: userI | undefined }) {
                 id="free-solo-demo"
                 freeSolo options={[]}
                 value={projectName}
-                onInputChange={(e) => setProjectName}
+                onInputChange={(event, newValue) => setProjectName(newValue)}
                 renderInput={(params) => <TextField className={styles.addInput} {...params} label="輸入專案名稱" />}
               />
             </div>
@@ -134,7 +156,7 @@ function CreateProject({ user }: { user: userI | undefined }) {
                     type="date"
                     id="end-date"
                     value={startDate}
-                    onChange={handleEndDateChange}
+                    onChange={handleStartDateChange}
                   />
                 </div>
                 <div className={`${styles.shortInput} ${styles.dateInput}`}>
@@ -170,7 +192,8 @@ function CreateProject({ user }: { user: userI | undefined }) {
             </div>
           </section>
           <section className={styles.setingButton}>
-            <a className={`${styles.button} ${styles.check}`}><FaSignOutAlt /></a>
+            <a className={`${styles.button} ${styles.check}`} onClick={handleSubmit}><FaCheck /></a>
+            <a className={`${styles.button} ${styles.delete}`} onClick={() => router.push('/projectManage/list')}><FaSignOutAlt /></a>
           </section>
         </main>
       </Layout>
